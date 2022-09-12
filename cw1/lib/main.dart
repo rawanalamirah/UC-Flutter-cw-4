4446234152a55be21351e749152f95935cb98c78
rawanalamirah <rawanalamirah@hotmail.com>
2022-09-12 19:31:42 +0300

    done

---
+++ cw1/lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              child: Row(
+                children: [
+                  Center(
+                    child: Image.asset(
+                      'asset/fav_animal.jpg',
+                      width: 200,
+                      height: 200,
+                    ),
+                  ),
+                  Text(
+                    'Type: Camels',
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              height: 400,
+              margin: EdgeInsets.all(10),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Name: Llama',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  Text(
+                    'Age: 8 yrs old',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  Text(
+                    'Gender: Female',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
